3b88d4d5d400b61c5111cb2f6dfb6d28e491ae0c
gtgaleevtimur <gtgaleevtimur@gmail.com>
2022-11-05 23:26:58 +0300

    some tests

--- internal/handlers/middlewares/cookies.go
+++ internal/handlers/middlewares/cookies.go
@@ -1,8 +1,9 @@
 package middlewares
 
 import (
-	"crypto/aes"
+	"crypto/hmac"
 	"crypto/rand"
+	"crypto/sha256"
 	"encoding/hex"
 	"log"
 	"net/http"
@@ -14,16 +15,16 @@ import (
 func CookiesMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		//Срез байт в качестве буфера.
-		authCookie := make([]byte, aes.BlockSize)
+		//authCookie := make([]byte, aes.BlockSize)
 		//Ключ шифрования.
 		key := []byte("AsdFrGtHyJhjErTy")
 		//Проверочное слово.
-		nonce := []byte("cookie")
+		//nonce := []byte("cookie")
 		//Инициализация интерфейся для симметричного шифрования.
-		aesBlock, err := aes.NewCipher(key)
-		if err != nil {
-			log.Println(err)
-		}
+		//aesBlock, err := aes.NewCipher(key)
+		//if err != nil {
+		//	log.Println(err)
+		//}
 		//Проверка наличия cookie в запросе.
 		if userCookie, err := r.Cookie("shortener"); err == nil {
 			//Расшифровка значения cookie в срез байт.
@@ -31,26 +32,38 @@ func CookiesMiddleware(next http.Handler) http.Handler {
 			if err != nil {
 				log.Printf("Cookie decoding: %v\n", err)
 			}
-			//Расшифровываем полученный срез байт с помощью интерфейса симметричного шифрования.
-			aesBlock.Decrypt(authCookie, userCookieByte)
+			//Расшифровываем полученный срез байт с помощью интерфейса симметричного шифрования.z`
+			//aesBlock.Decrypt(authCookie, userCookieByte)
+			h := hmac.New(sha256.New, key)
+			h.Write(userCookieByte)
+			sign := h.Sum(nil)
 			//Проверяем на подлинность сравнением конца расшированного означения от длинны проверчного слова и проверчного слова.
-			if string(authCookie[len(authCookie)-len(nonce):]) == string(nonce) {
+			//if string(authCookie[len(authCookie)-len(nonce):]) == string(nonce) {
+			//next.ServeHTTP(w, r)
+			//return
+			//}
+			if hmac.Equal(userCookieByte, sign) {
 				next.ServeHTTP(w, r)
 				return
 			}
 		}
 		//Если cookie нет или проверка на подлинность не пройдена,создаем новую cookie.
 		//Генерация userID.
-		userID, err := generateRandom(10)
+		userID, err := generateRandom(16)
 		if err != nil {
 			log.Printf("UserID generate: %v\n", err)
 		}
-		//Шифрование userID+проверочное слово.
-		aesBlock.Encrypt(authCookie, append(userID, nonce...))
-		//Инициализация и вставка новой cookie.
+		/*
+			//Шифрование userID+проверочное слово.
+			aesBlock.Encrypt(authCookie, append(userID, nonce...))
+			//Инициализация и вставка новой cookie.
+		*/
+		h := hmac.New(sha256.New, key)
+		h.Write(userID)
+		cook := h.Sum(nil)
 		cookie := &http.Cookie{
 			Name:    "shortener",
-			Value:   hex.EncodeToString(authCookie),
+			Value:   hex.EncodeToString(cook),
 			Expires: time.Now().Add(time.Hour * 24),
 		}
 		http.SetCookie(w, cookie)
