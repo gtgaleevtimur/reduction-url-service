f96955b69ec54f9b99d5c9a17620e8e7a9238ccf
gtgaleevtimur <gtgaleevtimur@gmail.com>
2023-01-05 15:20:22 +0300

    fuck this

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -1,13 +1,10 @@
 package config
 
-import (
-	"strconv"
-	"testing"
-)
-
+/*
 func BenchmarkConfig_ExpShortURL(b *testing.B) {
 	c := NewConfig(WithParseEnv())
 	for i := 0; i < b.N; i++ {
 		c.ExpShortURL(strconv.Itoa(i))
 	}
 }
+*/
