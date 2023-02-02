cdbfa22ae54c7e9eaf1b2597243ba39c8018fe27
gtgaleevtimur <gtgaleevtimur@gmail.com>
2023-02-02 13:46:03 +0300

    added configJSON

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,7 +1,9 @@
 package config
 
 import (
+	"encoding/json"
 	"flag"
+	"io/ioutil"
 	"strings"
 
 	"github.com/caarlos0/env"
@@ -16,11 +18,12 @@ const (
 
 // Config - структура конфигурационного файла приложения.
 type Config struct {
-	ServerAddress string `env:"SERVER_ADDRESS"`
-	BaseURL       string `env:"BASE_URL"`
-	StoragePath   string `env:"FILE_STORAGE_PATH"`
-	DatabaseDSN   string `env:"DATABASE_DSN"`
-	EnableHTTPS   bool   `env:"ENABLE_HTTPS"`
+	ServerAddress string `json:"server_address" env:"SERVER_ADDRESS"`
+	BaseURL       string `json:"base_url" env:"BASE_URL"`
+	StoragePath   string `json:"file_storage_path" env:"FILE_STORAGE_PATH"`
+	DatabaseDSN   string `json:"database_dsn" env:"DATABASE_DSN"`
+	EnableHTTPS   bool   `json:"enable_https" env:"ENABLE_HTTPS"`
+	Config        string `env:"CONFIG"`
 }
 
 // NewConfig - конструктор конфигурационного файла.
@@ -30,12 +33,35 @@ func NewConfig(options ...Option) *Config {
 		BaseURL:       HostAddr + ":" + HostPort,
 		StoragePath:   "",
 		DatabaseDSN:   "",
-		EnableHTTPS:   false,
+		Config:        "",
 	}
 
 	// если в аргументах получили Options, то применяем их к Config.
 	for _, opt := range options {
 		opt(&conf)
+	}
+	configDataJSON, err := ioutil.ReadFile(conf.Config)
+	if err != nil {
+		return &conf
+	}
+	var configJSON Config
+	if err = json.Unmarshal(configDataJSON, &configJSON); err != nil {
+		return &conf
+	}
+	if conf.ServerAddress == "" {
+		conf.ServerAddress = configJSON.ServerAddress
+	}
+	if conf.BaseURL == "" {
+		conf.BaseURL = configJSON.ServerAddress
+	}
+	if conf.StoragePath == "" {
+		conf.StoragePath = configJSON.StoragePath
+	}
+	if conf.DatabaseDSN == "" {
+		conf.DatabaseDSN = configJSON.StoragePath
+	}
+	if !conf.EnableHTTPS {
+		conf.EnableHTTPS = configJSON.EnableHTTPS
 	}
 	return &conf
 }
@@ -58,6 +84,7 @@ func (c *Config) ParseFlags() {
 	flag.StringVar(&c.StoragePath, "f", c.StoragePath, "FILE_STORAGE_PATH")
 	flag.StringVar(&c.DatabaseDSN, "d", c.DatabaseDSN, "DATABASE_DSN")
 	flag.BoolVar(&c.EnableHTTPS, "s", c.EnableHTTPS, "ENABLE_HTTPS")
+	flag.StringVar(&c.Config, "c", c.Config, "config JSON file")
 	flag.Parse()
 }
 
